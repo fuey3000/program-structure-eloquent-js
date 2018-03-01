9f9177565e97db1bf2b4fbf47387c7a0a458122c
Sal Gonzales <fuey3000@gmail.com>
2018-03-01 14:07:38 -0800

    chapter. 2 finished exercises

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # program-structure-eloquent-js
 Ch. 2 Program structure Eloquent JS.
+Finished exercises 1 and 2 in Chapter 2.
--- program-structure.js
+++ program-structure.js
@@ -1,1 +1,20 @@
+// Exercise 1 looping a triangle
 
+var s = '';
+for (var i = 0; i < 7; i++) {
+	s += '#';
+	console.log(s);
+}
+
+// Exercise 2 FizzBuzz
+
+for (var i = 1; i < = 100; i++) {
+    if (i % 15 === 0)
+        console.log('FizzBuzz');
+    else if (i % 3 === 0)
+        console.log('Fizz');
+    else if (i % 5 === 0)
+        console.log('Buzz');
+    else
+        console.log(i);
+}
